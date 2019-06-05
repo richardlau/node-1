b3b9e7ed0dabab83368755a814dbb109f666dd97
Richard Lau <riclau@uk.ibm.com>
2019-06-05 13:11:04 -0400

    test: fix test-console-clear for dumb terminals

--- test/parallel/test-console-clear.js
+++ test/parallel/test-console-clear.js
@@ -6,7 +6,7 @@ const assert = require('assert');
 const stdoutWrite = process.stdout.write;
 
 // The sequence for moving the cursor to 0,0 and clearing screen down
-const check = '\u001b[1;1H\u001b[0J';
+const check = process.env.TERM !== 'dumb' ? '\u001b[1;1H\u001b[0J' : '';
 
 function doTest(isTTY, check) {
   let buf = '';
