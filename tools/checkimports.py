0f8778ce355a598288c85270f46e5115109001ab
Richard Lau <riclau@uk.ibm.com>
2020-08-03 07:09:22 -0400

    fixup! temp debug

--- tools/checkimports.py
+++ tools/checkimports.py
@@ -46,7 +46,7 @@ if __name__ == '__main__':
     files = []
     for pattern in sys.argv[1:]:
       files = itertools.chain(files, glob.iglob(pattern))
-    print(files)
+    print(sys.argv[1:])
   else:
     files = glob.iglob('src/*.cc')
-  sys.exit(0 if all(map(is_valid, files)) else 1)
+  sys.exit(0 if all(list(map(is_valid, files))) else 1)
