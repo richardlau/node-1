nodejs/node

deps: patch V8 13.4 to support compilation with MSVC

deps/v8/src/maglev/maglev-graph-builder.cc
@@ -10206,13 +10206,19 @@ MaybeReduceResult MaglevGraphBuilder::TryReduceCallForNewClosure(
       return BuildCallRuntime(Runtime::kThrowConstructorNonCallableError,
                               {target_node});
     }
+
+#ifdef V8_ENABLE_LEAPTIERING
+      RETURN_IF_DONE(TryBuildCallKnownJSFunction(
+        target_context, target_node,
+        GetRootConstant(RootIndex::kUndefinedValue),
+        dispatch_handle,
+        shared, feedback_vector, args, feedback_source));
+#else
     RETURN_IF_DONE(TryBuildCallKnownJSFunction(
         target_context, target_node,
         GetRootConstant(RootIndex::kUndefinedValue),
-#ifdef V8_ENABLE_LEAPTIERING
-        dispatch_handle,
-#endif
         shared, feedback_vector, args, feedback_source));
+#endif
   }
   return BuildGenericCall(target_node, Call::TargetType::kJSFunction, args);
 }

richardlau/node-1

deps/v8/src/maglev/maglev-graph-builder.cc
@@ -11003,13 +11003,19 @@ MaybeReduceResult MaglevGraphBuilder::TryReduceCallForNewClosure(
       return BuildCallRuntime(Runtime::kThrowConstructorNonCallableError,
                               {target_node});
     }
+
+#ifdef V8_ENABLE_LEAPTIERING
+      RETURN_IF_DONE(TryBuildCallKnownJSFunction(
+        target_context, target_node,
+        GetRootConstant(RootIndex::kUndefinedValue),
+        dispatch_handle,
+        shared, feedback_cell, args, feedback_source));
+#else
     RETURN_IF_DONE(TryBuildCallKnownJSFunction(
         target_context, target_node,
         GetRootConstant(RootIndex::kUndefinedValue),
-#ifdef V8_ENABLE_LEAPTIERING
-        dispatch_handle,
-#endif
         shared, feedback_cell, args, feedback_source));
+#endif
   }
   return BuildGenericCall(target_node, Call::TargetType::kJSFunction, args);
 }