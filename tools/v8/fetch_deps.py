caf33a5e4d0da21ecfe561e62b1e24e290e25a3c
Richard Lau <richard.lau@ibm.com>
2026-09-09 15:15:04 +0000

    tools: update `depot_tools` override
    Signed-off-by: Richard Lau <richard.lau@ibm.com>

--- tools/v8/fetch_deps.py
+++ tools/v8/fetch_deps.py
@@ -25,7 +25,7 @@ GCLIENT_SOLUTION = [
     "managed"     : False,
     "custom_deps" : {
       # Update depot_tools for compatibility with Python 3.12.
-      "v8/third_party/depot_tools"            : "https://chromium.googlesource.com/chromium/tools/depot_tools.git@284c5ccb591c3de4e9f71be4a4beb5d1916d5383",
+      "v8/third_party/depot_tools"            : "https://chromium.googlesource.com/chromium/tools/depot_tools.git@da08833a5dfea59cd32248f0ed27a60cfd310e40",
       # These deps are already part of Node.js.
       "v8/base/trace_event/common"            : None,
       "v8/third_party/abseil-cpp"             : None,
